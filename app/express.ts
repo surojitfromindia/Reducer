7201f8fdc761b06e8ab0248c0704803ae0dd935b
Surojit Paul <surojit99923@gmail.com>
2022-09-11 20:44:33 +0530

    Password validation
    Password validation before register fix.

--- app/express.ts
+++ app/express.ts
@@ -1,16 +1,16 @@
 import * as express from 'express';
-import { cors } from './api/express/middlewares/cors';
 import { errorHandler } from './api/express/middlewares/errorHandler';
 import { UserRegisterValidate, Register } from './api/express/v1/register';
 import { Login } from './api/express/v1/login';
 import { sendCustomError } from './errors/errorMessages';
 import "./config/passport";
+import * as cors from "cors";
 
 const express_app = express.default();
 
 express_app.response.sendCustomError = sendCustomError;
-express_app.use(cors());
 express_app.use(express.json())
+express_app.use(cors.default());
 
 express_app.get('/register/validateuser', UserRegisterValidate);
 express_app.post('/register', Register);
